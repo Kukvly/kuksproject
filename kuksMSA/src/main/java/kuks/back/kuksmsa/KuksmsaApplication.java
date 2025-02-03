e35b961f8b9b57fb45ee6b827fd0533c87b503c8
Kukvly <kukvly96@gmail.com>
2025-02-03 23:49:39 +0900

    kuksmsa setting

---
+++ kuksMSA/src/main/java/kuks/back/kuksmsa/KuksmsaApplication.java
@@ -0,0 +1,13 @@
+package kuks.back.kuksmsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuksmsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KuksmsaApplication.class, args);
+    }
+
+}
